7ce26f53dd337481df117d27ee368af621329589
perrymaro2010-png <perrymaro2010@gmail.com>
2026-08-05 00:32:43 +0300

    feat: connect to MongoDB database found in MONGO_URI

---
+++ db/connect.js
@@ -0,0 +1,15 @@
+const config = require('../config/config');
+const mongoose = require('mongoose');
+const connectDB = async ()=>{
+    console.log("Connecting to MongoDB...");
+    try{
+        await mongoose.connect(config.mongoURI);
+        console.log('Connection success!');
+    } catch (err) {
+        console.warn('Connection failure!');
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
