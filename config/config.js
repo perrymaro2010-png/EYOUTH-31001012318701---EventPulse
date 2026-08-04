83f249411715aa5d9cc0ed6fcbaf9a4d15480773
perrymaro2010-png <perrymaro2010@gmail.com>
2026-08-05 00:31:06 +0300

    feat: adding config to access the .env file through one file

---
+++ config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const required = ['MONGO_URI','JWT_SECRET','JWT_EXPIRES_IN'];
+const missing = required.filter((key)=> !process.env[key]);
+
+if(missing.length > 0){
+    console.error(`Missing .env variables: ${missing}`);
+    process.exit(1);
+}
+
+module.exports = {
+    port: process.env.PORT || 3000,
+    mongoURI: process.env.MONGO_URI,
+    isDev: process.env.NODE_ENV === "development",
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpires: process.env.JWT_EXPIRES_IN
+};
