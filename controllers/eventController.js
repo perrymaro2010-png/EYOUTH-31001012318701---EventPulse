6b6ed23505ceb4b93fef92e0b0225198f24ccdd9
perrymaro2010-png <perrymaro2010@gmail.com>
2026-08-06 21:32:55 +0300

    feat: adding eventController to manipulate events, filter them, and search them, too

---
+++ controllers/eventController.js
@@ -0,0 +1,100 @@
+const {asyncHandler, ok} = require('../utils/asyncHandler');
+const AppError = require('../utils/AppError');
+const Event = require('../models/eventModel');
+
+// GET - /api/events
+const listEvents = asyncHandler(async (req, res)=> {
+    const {category, city, dateFrom, dateTo, search} = req.query;
+    let filter = {};
+
+    if(category) filter.category = category;
+    if(city) filter.city = city;
+    if(dateFrom || dateTo){
+        filter.date = {};
+        if(dateFrom) 
+            filter.date.$gte = new Date(dateFrom);
+        if(dateTo) 
+            filter.date.$lte = new Date(dateTo);
+    };
+    if (search) {
+    //--Text Search--
+    filter.$or = [
+      {
+        name: {
+          $regex: req.query.search,
+          $options: "i",
+        },
+      },
+      {
+        description: {
+          $regex: req.query.search,
+          $options: "i",
+        },
+      },
+    ]};
+    //--Pagination--
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const pageNum = page >= 1 && page <= 10
+    ? page
+    : 1
+    const limitNum = limit >= 1 && limit <= 50
+    ? limit
+    : 10
+
+    const skip = (pageNum - 1) * limitNum;
+
+
+    //--Sorting--
+    const sortBy = req.query.sortBy === 'date' ? 'date' : 'busiest';
+    const order = req.query.order === 'desc' ? -1 : 1;
+    const sort = {[sortBy]: order}
+    
+    
+    const [events, total] = await Promise.all([
+        Event.find(filter).populate('category').sort(sort).skip(skip).limit(limitNum),
+        Event.countDocuments(filter)
+    ]);
+
+    ok(res, {
+        events,
+        pagination:{
+            page: pageNum,
+            limit: limitNum,
+            totalResults: total,
+            totalPages: Math.ceil(total / limitNum)
+        }
+    }, 'Events Fetched Successfully');
+});
+
+// GET /api/events/:id
+const getEvent = asyncHandler(async (req, res)=>{
+    const {id} = req.params;
+    const event = await Event.findById(id).populate('category');
+    if (!event) 
+        throw new AppError('Event Not Found', 404);
+    ok(res, event, 'Event Fetched Successfully');
+});
+// POST - /api/events
+const createEvent = asyncHandler(async (req, res)=> {
+    const newEvent = await Event.create({...req.body, organiser: req.user._id});
+    await newEvent.populate('category');
+    ok(res, newEvent, 'New Event Created Successfully', 201);
+});
+
+// PATCH - /api/events/:id
+const updateEvent = asyncHandler(async (req, res)=>{
+    const {id} = req.params;
+    const event = await Event.findByIdAndUpdate(id, req.body, {new: true, runValidators: true}).populate('category');
+    if (!event) throw new AppError('Event Not Found', 404);
+    ok(res, event, 'Events Updated Successfully');
+});
+
+// DELETE /api/events/:id
+const deleteEvent = asyncHandler(async (req, res)=> {
+    const {id} = req.params;
+    const deletedEvent = await Event.findByIdAndDelete(id);
+    if(!deletedEvent) throw new AppError('Event Not Found', 404);
+    ok(res, null, 'Event Deleted Successfully');
+});
+
