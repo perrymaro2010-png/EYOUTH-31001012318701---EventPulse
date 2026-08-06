79dec116ef8afe4c5a4c755217ca00fd83a395ca
perrymaro2010-png <perrymaro2010@gmail.com>
2026-08-06 21:41:30 +0300

    feat: validating existent user, and authenticating for roles according to JWT

---
+++ middleware/requireAuth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('../utils/AppError');
+const User = require('../models/userModel');
+const config = require('../config/config');
+
+const protect = asyncHandler(async (req, res, next)=> {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer '))
+        throw new AppError('No token provided', 401);
+
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, config.jwtSecret);
+    const user = await User.findById(decoded.id);
+    if (!user) throw new AppError('User not found', 404);
+    req.user = user;
+
+    next();
+
+});
+
+const requireRole = (...roles)=>{
+    return (req, res, next)=>{
+        if (!roles.includes(req.user.role))
+            throw new AppError('You are not allowed to perform this action', 403);
+        next();
+    } 
+};
+module.exports = {protect, authorize};
