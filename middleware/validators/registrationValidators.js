2db15776bfc44034af4f8a906361755fb56fec33
perry <perrymaro2010@gmail.com>
2026-08-17 23:13:00 +0300

    fix: renaming file name

---
+++ middleware/validators/registrationValidators.js
@@ -0,0 +1,29 @@
+const {body, query, param, validationResult} = require('express-validator');
+const AppError = require("../utils/AppError.js");
+
+const validateIDinBody = [
+    body('eventID')
+    .notEmpty().withMessage('Event ID is required')
+    .isMongoId().withMessage('Invalid Event ID'),
+];
+
+const validateIDinParam = [
+    param('eventID')
+    .notEmpty().withMessage('Event ID is required')
+    .isMongoId().withMessage('Invalid Event ID')
+];
+
+const validator = (req, res, next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const message = errors.array().map((e)=> e.msg).join(', ');
+        return next(new AppError(message, 400))
+    }
+    next();
+};
+
+module.exports = {
+    validateIDinBody,
+    validateIDinParam,
+    validator
+};
