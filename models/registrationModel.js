b93820a834c8589d065875bde5e61b195fa42826
perry <perrymaro2010@gmail.com>
2026-08-17 22:37:49 +0300

    fix: renaming 'user' to 'attendee', removing status, applying indexing, swapping almost all mentions of 'bookings' to 'registrations' to follow naming conventions

---
+++ models/registrationModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const registrationSchema = new mongoose.Schema({
+    attendee:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User name is required']
+    },
+    event:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+        required: [true, 'Event field is required']
+    }
+}, {timestamps: true});
+
+
+registrationSchema.index({event: 1, attendee: 1}, {unique: true});
+
+const Registration = mongoose.model('Registration', registrationSchema);
+module.exports = Registration;
