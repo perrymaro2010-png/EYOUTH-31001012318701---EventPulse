e70d8fb3a94df7b52bf86b685fb5ac2d47d996fb
perry <perrymaro2010@gmail.com>
2026-08-17 20:56:56 +0300

    feat: app.js, where everything is connected to work on the project

---
+++ app.js
@@ -0,0 +1,38 @@
+const config = require('../config/config');
+const express = require('express');
+const app = express();
+const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
+const connectDB = require('../db/connect');
+
+const authRoutes = require('./routes/authRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
+app.use(cors());
+app.use(express.json()); 
+app.use(mongoSanitize());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/bookings', bookingRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ status: 'error', message: 'Route not found' });
+});
+
+app.use(errorHandler);
+
+const start = async ()=> {
+    try {
+        await connectDB();
+        app.listen(config.port, () => {
+            console.log(`Server running on port ${config.port}`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+start();
