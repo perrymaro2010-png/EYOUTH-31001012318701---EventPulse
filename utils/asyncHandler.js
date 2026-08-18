67e1d35658d81e2daaf1d63e9d3d87c21e9f79b5
perry <perrymaro2010@gmail.com>
2026-08-18 19:47:39 +0300

    fix: add an okList function to cater to lists (especially with the details about pagination and sorting) in the eventController

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -3,7 +3,10 @@ const asyncHandler = (fn) => (req, res, next)=>{
 };
 
 const ok = (res, data, message = 'success', code = 200) =>{
-    res.status(code).json({status: code, message, data});
+    res.status(code).json({status: 'success', message, data});
 };
 
-module.exports = {asyncHandler, ok};
+const okList = (res, {data, page, limit, totalResults, totalPages}, code = 200) =>{
+    res.status(code).json({status: 'success', page, limit, totalResults, totalPages, data});
+}
+module.exports = {asyncHandler, ok, okList};
