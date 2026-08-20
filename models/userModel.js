7706818952c721dfbd19d278b3e040b88ded8206
perry <perrymaro2010@gmail.com>
2026-08-20 12:26:45 +0300

    fix: remove unnecessary (next)

--- models/userModel.js
+++ models/userModel.js
@@ -31,12 +31,11 @@ const userSchema = new mongoose.Schema({
     toObject: true
 });
 
-userSchema.pre('save', async function(next){
+userSchema.pre('save', async function(){
     if(!this.isModified('password'))
         return next();
     
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 });
 
 userSchema.methods.comparePassword = async function (pass) {
