d03576ec6150790dfc94cef9b0132eaa1c9deb95
perry <perrymaro2010@gmail.com>
2026-08-20 10:58:45 +0300

    fix: removing the message handling to put in a separate file

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -20,17 +20,11 @@ const {
     deleteEvent
 } = require('../controllers/eventController');
 
-const { listMessages } = require('../controllers/messageController');
-
 // event handling
 router.get('/', validateQuery, validator, listEvents);
 router.get('/:id', validateID, validator, getEvent);
 router.post('/', requireAuth, requireRole('admin'), validateAllBody, validator, createEvent);
 router.patch('/:id', requireAuth, requireRole('admin'), validateID, validatePartialBody, validator, updateEvent);
 router.delete('/:id', requireAuth, requireRole('admin'), validateID, validator, deleteEvent);
-
-
-// message handling
-router.get('/:id/messages', requireAuth, validateID, validator, listMessages);
 
 module.exports = router;
